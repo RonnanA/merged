ead967dd0eae32f7d234214dec371f572ca6611b
RonnanA <ronnanalexander@hotmail.com>
2026-03-23 13:49:05 -0400

    add prompt.txt file for ollama model, create get_code_review function to get response from ollama model based on sample prompt

---
+++ src/reviewer.py
@@ -0,0 +1,20 @@
+from ollama import chat
+
+def get_code_review():
+    prompt_file = 'src/prompt.txt'
+
+    with open(prompt_file, 'r') as f:
+        prompt_text = f.read()
+
+    print(f"prompt text: {prompt_text}")
+
+    response = chat(model='qwen2.5-coder:1.5b', messages=[
+        {
+            'role': 'user',
+            'content': prompt_text,
+        },
+    ])
+    
+    pr_comment = response.message.content
+    return pr_comment
+
