f74c78a761f13ddbe93485a089e4937184b80f55
RonnanA <ronnanalexander@hotmail.com>
2026-03-23 00:20:47 -0400

    added the get pr diff request

--- src/github_client.py
+++ src/github_client.py
@@ -4,12 +4,14 @@ import sys
 import os
 
 def get_diff(github_token):
+    # Get the owner and repo value from GITHUB_REPOSITORY ENV var
     github_repository = os.getenv('GITHUB_REPOSITORY')
     if not github_repository:
         print("Error: GITHUB_REPOSITORY env var not set")
         sys.exit(1)
     repo = github_repository.split("/")
 
+    # Get the PR number from the GITHUB_EVENT_PATH ENV var
     github_event_path = os.getenv('GITHUB_EVENT_PATH')
     if not github_event_path:
         print("Error: GITHUB_EVENT_PATH env var not set")
@@ -21,8 +23,7 @@ def get_diff(github_token):
 
             if pr_number:
                 print(f"Successfully found the PR number: {pr_number}")
-
-            
+  
     except FileNotFoundError:
         print(f"Error: GitHub event file not found at {github_event_path}")
         sys.exit(1)
@@ -30,4 +31,20 @@ def get_diff(github_token):
         print(f"Error: Could not decode JSON from the GitHub event file at {github_event_path}")
         sys.exit(1)
 
-    print(f"GET https://api.github.com/repos/{repo[0]}/{repo[1]}/pulls/{pr_number}")
+
+    api_url = f"https://api.github.com/repos/{repo[0]}/{repo[1]}/pulls/{pr_number}"
+    headers = {
+        'Accept': '"application/vnd.github.v3.diff"',
+        'Authorization': f'token {github_token}',
+        'X-GitHub-Api-Version': '2026-03-10'
+    }
+    try:
+        response = requests.get(api_url, headers=headers)
+        pr_diff = response.text
+        print(pr_diff)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        if response is not None:
+            print(f"Response status code: {response.status_code}")
+            print(f"Response body: {response.text}")
