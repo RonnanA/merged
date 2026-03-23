a9c42ee72627fc0eac3d0040bf0b404f39ec8ec2
RonnanA <ronnanalexander@hotmail.com>
2026-03-23 09:25:25 -0400

    add delete_previous_comment function and check json type

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,7 +1,7 @@
 import time
 import sys
 import os
-from github_client import get_diff, post_comment
+from github_client import get_diff, post_comment, delete_previous_comment
 
 
 github_token = sys.argv[1]
@@ -23,4 +23,7 @@ if github_output:
 repo, pr_number, pr_diff = get_diff(github_token)
 
 sample_text = "sample comment for PR"
-post_comment(github_token, repo, pr_number, sample_text)
+#post_comment(github_token, repo, pr_number, sample_text)
+
+delete_previous_comment(github_token, repo, pr_number)
+
--- src/github_client.py
+++ src/github_client.py
@@ -47,7 +47,7 @@ def get_diff(github_token):
         print(pr_diff)
 
     except requests.exceptions.RequestException as e:
-        print(f"Error posting comment: {e}")
+        print(f"Error getting diff: {e}")
         print(f"Response status code: {response.status_code}")
         print(f"Response body: {response.text}")
 
@@ -80,4 +80,25 @@ def post_comment(github_token, repo, pr_number, review_text):
 
     print("post comment function complete")
 
-#def delete_previous_comment(github_token, repo, pr_number):
+def delete_previous_comment(github_token, repo, pr_number):
+    api_url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
+
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {github_token}',
+        'X-GitHub-Api-Version': '2026-03-10'
+    }
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()
+
+        comments_data = response.json()
+        comment_id = comments_data.get('id')
+        comment_body = comments_data.get('body')
+        print(f"Comment id: {comment_id}")
+        print(f"Comment body: {comment_body}")
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error deleting comment: {e}")
+        print(f"Response status code: {response.status_code}")
+        print(f"Response body: {response.text}")
