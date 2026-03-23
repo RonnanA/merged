a40e4c8b32a8ec71cb7b6222c0b34ceff66a932d
RonnanA <ronnanalexander@hotmail.com>
2026-03-23 13:57:47 -0400

    fix path again

--- src/reviewer.py
+++ src/reviewer.py
@@ -1,7 +1,7 @@
 from ollama import chat
 
 def get_code_review():
-    prompt_file = 'prompt.txt'
+    prompt_file = '/src/prompt.txt'
 
     with open(prompt_file, 'r') as f:
         prompt_text = f.read()
